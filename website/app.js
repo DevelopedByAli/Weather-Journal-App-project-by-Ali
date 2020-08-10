4d8ecddd45fc01437e4aa6036806e1e12ddfff73
Akbarali <akbarali2191@gmail.com>
2020-08-10 23:07:38 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -37,3 +37,26 @@ const getWeather = async(baseUrl, zip, key) => {
         console.log('error', error);
     }
 }
+// Posting data function 
+const addData = async(url = '', data = {}) => {
+
+    console.log(data);
+    const response = await fetch(url, {
+
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+
+        body: JSON.stringify(data)
+    })
+
+    try {
+        const newData = await response.json();
+        console, log(newData);
+        return newData
+    } catch (error) {
+        console.log('error', error);
+    }
+}
