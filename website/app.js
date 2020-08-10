f58da4e0fb40ef2b754567447baa86748c8cde02
Akbarali <akbarali2191@gmail.com>
2020-08-10 22:59:55 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,4 +1,9 @@
 /* Global Variables */
+// OpenWheatherMap API key 
+
+let baseUrl = 'http://api.openweathermap.org/data/2.5/forecast?zip=';
+const apiKey = '&appid=09af6e7123579bd866598c0c0973661a';
+const generate = document.getElementById('generate');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
